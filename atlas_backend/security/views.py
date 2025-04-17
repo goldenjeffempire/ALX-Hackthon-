ee39d2254be2f6573de7531ca0951674e06ea0fe
goldenjeffempire <jeffemuodafe124@gmail.com>
2025-04-17 05:23:35 +0100

    Implemented the Security Models

--- atlas_backend/security/views.py
+++ atlas_backend/security/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import AuditLog
+from .serializers import AuditLogSerializer
+from .permissions import IsAdminOrReadOnly
 
-# Create your views here.
+class AuditLogViewSet(viewsets.ReadOnlyModelViewSet):
+    serializer_class = AuditLogSerializer
+    permission_classes = [IsAdminOrReadOnly]
+
+    def get_queryset(self):
+        return AuditLog.objects.all()
