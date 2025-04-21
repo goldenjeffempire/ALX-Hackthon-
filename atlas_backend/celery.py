d977dbc45600e877addff55eedfe149d1328d3b9
goldenjeffempire <jeffemuodafe124@gmail.com>
2025-04-21 23:43:00 +0100

    Fixed Error

--- atlas_backend/celery.py
+++
@@ -1,8 +0,0 @@
-import os
-from celery import Celery
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "atlas_backend.settings")
-
-app = Celery("atlas_backend")
-app.config_from_object("django.conf:settings", namespace="CELERY")
-app.autodiscover_tasks()
